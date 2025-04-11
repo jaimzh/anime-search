fa0a521357a978eb30962245e13b7ea2b7baee40
Jaimz <jaimz@gmail.com>
2025-04-11 18:33:36 +0100

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/anime-search-site/"
-  // base: process.env.VITE_BASE_PATH || "/anime-search-site",
+  // base: "/anime-search-site/"
+  base: process.env.VITE_BASE_PATH || "/anime-search-site",
 })
